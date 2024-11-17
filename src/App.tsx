308df730f2076b99ccad5cd0638baa0db2a1c52d
yoodi <ashleyyoodikim@gmail.com>
2024-11-17 01:07:07 -0800

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, RouterProvider, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import "@mantine/core/styles.css";
 import { Flex, MantineProvider} from "@mantine/core";

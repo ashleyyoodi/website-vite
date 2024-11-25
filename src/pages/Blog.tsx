fa4515ab267111f66265477555e028cd68e7b014
yoodi <ashleyyoodikim@gmail.com>
2024-11-24 16:44:19 -0800

    load all blog posts and add page headers

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -3,23 +3,30 @@ import { useEffect, useState } from "react";
 
 export default function() {
     const [isLoading, setIsLoading] = useState(false);
-    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState<any[]>([]);
 
     useEffect(() => {
         setIsLoading(true);
         fetch('https://website-backend-beta-bay.vercel.app/blog-posts')
         .then(response => response.json())
         .then(data => {
-            setMessage(data[0].text);
+            setMessages(data);
             setIsLoading(false);
         });
     }, []);
     
     return (
         <div>
-            <Text>
-                {message}
-            </Text>
+            <h2 className="page-header">Blog</h2>
+            <br />  
+            { messages.map((message) => (
+                <div className = "blog-post">
+                    <Text>
+                        {message.text}
+                    </Text>
+                    <br />
+                </div>
+            ))}
             { isLoading ?
                 <div className="loader-container">
                     <Loader color="blue" />
--- src/pages/Photos.tsx
+++ src/pages/Photos.tsx
@@ -3,6 +3,8 @@ import { Text } from "@mantine/core";
 export default function() {
     return (
         <div>
+            <h2 className="page-header">Photos</h2>
+            <br />  
             <Text>
                 No photos to share yet.
             </Text>
